59bdbd2372bc272226212939c0e70c89cdab60d2
mcube25 <mcube3567@gmail.com>
2017-06-18 23:08:25 +0100

    starting pug

--- myexpress works/myapp.js
+++ myexpress works/myapp.js
@@ -8,3 +8,14 @@ app.set('port,3000');
 app.listen(3000,function(){
     console.log('myapp listening on port 3000');
 });
+//start pug
+var express=require('express');
+var app=express();
+app.set('view engine','pug');
+app.use('/static',express.static('public'));
+app.get('/',function(req,res){
+    res.render('index',{title:'mycode',message:'this is my message'});
+});
+app.listen(3000,function(){
+    console.log('listening on port 3000');
+});
