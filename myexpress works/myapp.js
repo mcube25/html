42052ed348885a2927bcbf91c66e8f1b5bdc6e04
mcube25 <mcube3567@gmail.com>
2017-06-21 00:18:25 +0100

    express old forms

--- myexpress works/myapp.js
+++ myexpress works/myapp.js
@@ -17,5 +17,18 @@ app.get('/',function(req,res){
     res.render('index',{title:'mycode',message:'this is my message'});
 });
 app.listen(3000,function(){
-    console.log('listening on port 3000');
+    conesole.log('listening on port 3000');
+});
+//express form
+var express=require('express');
+var app=express();
+var bodyParser=require('body-parser');
+app.use(bodyParser.urlencoded({
+    extended:false
+}))
+app.post('/form',function(req,res){
+    res.send(req.body.str.split('').reverse().join(''));
+})
+app.listen(3000,function(){
+    console.log('listening on port 3000')
 });
