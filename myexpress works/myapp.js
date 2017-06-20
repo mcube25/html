09a1b22460085db012eead67f35e17947865aaf8
mcube25 <mcube3567@gmail.com>
2017-06-21 00:48:58 +0100

    style css

--- myexpress works/myapp.js
+++ myexpress works/myapp.js
@@ -32,3 +32,12 @@ app.post('/form',function(req,res){
 app.listen(3000,function(){
     console.log('listening on port 3000')
 });
+//stylish css
+var express=require('express');
+var app=express();
+app.use(require('stylus').middleware(process.argv[3]));
+app.use(express.static(process.argv[3]));
+app.get(process.argv[3],function(req,res){
+    res.render('main');
+})
+app.listen(process.argv[2]);
