35756010a3f6017a4e77762358fa716554ff46d8
mcube25 <mcube3567@gmail.com>
2017-07-08 23:02:09 +0100

    set date params

--- timestamp project/timestamp.js
+++ timestamp project/timestamp.js
@@ -13,3 +13,46 @@ app.get('/', function(request, response) {
 app.listen(PORT, function() {
   console.log('Server is listening on port ' + PORT);
 });
+var express = require('express');
+ 
+var app = express();
+var PORT = process.env.PORT || 3000;
+ 
+app.get('/:timestamp', function(request, response) {
+	var timestamp = request.params.timestamp;
+	response.json(getTimestampJSON(timestamp));
+});
+ 
+app.listen(PORT, function() {
+	console.log('Server is listening on port ' + PORT);
+});
+ 
+function getTimestampJSON(timestamp) {
+	// First, we handle the unix timestamps. Path parameters come in as text
+	// rather than numbers, so we'll attempt to parse them.
+	var result = {
+		unix: null,
+		natural: null
+	};
+ 
+	var date;
+	if (!isNaN(parseInt(timestamp))) {
+		// This means that the parsed integer is NOT a
+		// NaN (not a number) value, in other words: it's a valid number.
+		date = new Date(parseInt(timestamp));
+	} else {
+		// Timestamp is not a valid number, we'll create a Date object
+		// and then check if is valid before we return it
+		date = new Date(timestamp);
+	}
+ 
+	if (!isNaN(date.getTime())) {
+		// Date.getTime() returns the unix timestamp,
+		// if it where an invalid date, this would be NaN
+		result.unix = date.getTime();
+		result.natural = 'We will get here eventually!';
+	}
+ 
+	// We return the value, whatever it may be.
+	return result;
+}
